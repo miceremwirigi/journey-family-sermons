e55463aebceb614e2b6035111422d4da6693e898
miceremwirigi <miceremwirigi@gmail.com>
2026-01-08 12:09:11 +0300

    fix download button text after a download

--- templates/app.js
+++ templates/app.js
@@ -565,7 +565,14 @@ async function downloadmp3(e, videoId){
         alert("Error: " + error.message)
     } finally {
         btn.disabled = false;
-        btn.innerText = "Convert to mp3"
+        btn.innerHTML = `
+                    MP3
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                        <polyline points="7 10 12 15 17 10"></polyline>
+                        <line x1="12" y1="15" x2="12" y2="3"></line>
+                    </svg>
+                    `
     }
 }
 
@@ -621,7 +628,15 @@ async function downloadmp4(e, videoId){
         alert("Error: " + error.message)
     } finally {
         btn.disabled = false;
-        btn.innerText = "Convert to mp4"
+        btn.innerHTML = `
+        
+                    MP4
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                        <polyline points="7 10 12 15 17 10"></polyline>
+                        <line x1="12" y1="15" x2="12" y2="3"></line>
+                    </svg>
+                    `
     }
 }
 // **********************************************************************************************//
